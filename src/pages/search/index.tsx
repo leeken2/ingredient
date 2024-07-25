900fed3671e196cfa17da65945b47ac9ad0673cc
yuanyang@77ircloud.com <yuanyang@ircloud.com>
2024-07-25 20:47:06 +0800

    chore: 页面分页调整

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -4,8 +4,10 @@ import "./index.less";
 import { data } from "./data";
 import { useState } from "react";
 
+const defaulePageSize = 30;
 export default function Index() {
   const [step, updateStep] = useState(0);
+  const [currPage, updateCurrPage] = useState(1);
   useLoad(() => {
     console.log("Page loaded.");
     console.log(data.length);
@@ -50,22 +52,22 @@ export default function Index() {
 
   const renderItem = (item) => {
     return (
-      <View className="card">
+      <View className="card" key={item["保鸿编号"]}>
         <View className="content">
           <View className="title">保鸿编号：</View>
-          <View className="value">{item['保鸿编号']}</View>
+          <View className="value">{item["保鸿编号"]}</View>
         </View>
         <View className="content">
           <View className="title">原厂色号：</View>
-          <View className="value">{item['原厂色号']}</View>
+          <View className="value">{item["原厂色号"]}</View>
         </View>
         <View className="content">
           <View className="title">颜色名称：</View>
-          <View className="value">{item['颜色名称']}</View>
+          <View className="value">{item["颜色名称"]}</View>
         </View>
         <View className="content">
           <View className="title">车名：</View>
-          <View className="value">{item['车名']}</View>
+          <View className="value">{item["车名"]}</View>
         </View>
       </View>
     );
@@ -74,15 +76,45 @@ export default function Index() {
   const renderList = () => {
     return (
       <View className="searh-list">
+        <View className="top">
+          <Text
+            onClick={(e) => {
+              updateStep(0);
+            }}
+          >
+            {"<"}
+          </Text>
+        </View>
         <ScrollView
           scroll-y
           className="scroll-view"
           style={{ height: "100vh" }}
         >
-          {data.splice(0, 10).map((item) => {
+          {data.splice(currPage, 30).map((item) => {
             return renderItem(item);
           })}
         </ScrollView>
+        <View className="bottom">
+          <View
+            onClick={() => {
+              if (currPage > 1) {
+                updateCurrPage(currPage - 1);
+              }
+            }}
+          >
+            上一页
+          </View>
+          <View>第{currPage}页</View>
+          <View
+            onClick={() => {
+              if (currPage <= data.length / defaulePageSize) {
+                updateCurrPage(currPage + 1);
+              }
+            }}
+          >
+            下一页
+          </View>
+        </View>
       </View>
     );
   };
